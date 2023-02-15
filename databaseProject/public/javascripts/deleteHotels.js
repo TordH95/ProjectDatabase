ff978ee57db5b43dfbf5539bf3e8c9e970dd466b
Tord Husjord <thusjord@msn.com>
2023-02-15 14:21:12 +0100

    Buttons for add/delete hotels + functions

---
+++ databaseProject/public/javascripts/deleteHotels.js
@@ -0,0 +1,22 @@
+async function addHotel(url, hotelid) {
+    console.log(url, hotelid)
+    await fetch(url, {
+        method: "DELETE",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            id: hotelid
+        })
+    }).then((response) => {
+        if (response.ok) {
+            const resData = "Hotel deleted";
+            location.reload()
+            return Promise.resolve(resData);
+        }
+        return Promise.reject(response);
+    })
+    .catch((response) => {
+        alert(response.statusText);
+    });
+}
