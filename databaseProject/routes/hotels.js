dec1f1936a65b5a20ac5c817e44daca8a8ce6b21
Tord Husjord <thusjord@msn.com>
2023-02-15 13:36:47 +0100

    Made the molder folder

---
+++ databaseProject/routes/hotels.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var HotelService = require("../services/HotelService")
+var db = require("../models");
+var hotelService = new HotelService(db);
+
+/* GET hotels listing. */
+router.get('/', async function(req, res, next) {
+  const hotels = await hotelService.get();
+  res.send(hotels);
+});
+
+module.exports = router;
